ae52b5f43a33648126c3175ad75356895c180439
Paolo Deidda <paolodeidda.97@gmail.com>
2026-04-14 02:01:22 +0200

    add author check to gittalo

--- gittalo.sh
+++ gittalo.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+EXPECTED_AUTHOR="DePoli97"
+CURRENT_AUTHOR=$(git config user.name)
+if [ "$CURRENT_AUTHOR" != "$EXPECTED_AUTHOR" ]; then
+  echo "Aborting: git user.name is '$CURRENT_AUTHOR', expected '$EXPECTED_AUTHOR'."
+  echo "Fix with: git config user.name \"$EXPECTED_AUTHOR\""
+  exit 1
+fi
 git pull
 git add .
-git commit -m "remote machine synchronization"
+git commit -m "auto remote machine synchronization"
 git push

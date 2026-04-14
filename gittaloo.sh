cb97b244e5415d618f08ead581e6d9d346712a13
DePoli97 <paolodeidda.97@gmail.com>
2026-04-14 23:02:51 +0200

    remote - quick repo synchronization

--- gittaloo.sh
+++ gittaloo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 CURRENT_AUTHOR=$(git config user.name)
 
 if [[ "$CURRENT_AUTHOR" != "Paolo Deidda" && "$CURRENT_AUTHOR" != "DePoli97" ]]; then
@@ -7,5 +8,5 @@ if [[ "$CURRENT_AUTHOR" != "Paolo Deidda" && "$CURRENT_AUTHOR" != "DePoli97" ]];
   exit 1
 fi
 
-# Concatenare i comandi con && è più sicuro e conciso: se uno fallisce, i successivi si fermano
-git pull && git add . && git commit -m "home - quick repo synchronization" && git push
+git pull && git add . && git commit -m "remote - quick repo synchronization" && git push
+

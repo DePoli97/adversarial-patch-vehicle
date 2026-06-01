b3583043130e54c5e6c2c461bfa9ff3f25320871
Paolo Deidda <paolodeidda.97@gmail.com>
2026-06-01 18:43:00 +0200

    remote - quick repo synchronization

--- src/chroma_key_dataset_generator/patch_overlay.sh
+++ src/chroma_key_dataset_generator/patch_overlay.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd ../..
+#cd ../..
 
 python3 src/chroma_key_dataset_generator/overlay_on_tga.py \
   --base assets/chroma_key/Vh_Truck_CarlaCola_BodyworkMat_BaseColor.TGA \
   --overlay assets/chroma_key/yellow_marker.TGA \
-  --x 2900 --y 2400 \
+  --x 2885 --y 2400 \
   --scale 0.8 \
   --rotate 0 \
   --output assets/chroma_key/carlacola_with_yellow.TGA
